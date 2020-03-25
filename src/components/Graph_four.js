69052eec01a66c3b62f9e1b56a90f411f4373825
unknown <amruta.banarase@saama.com>
2020-03-25 15:09:11 +0530

    Update graph code

--- src/components/Graph_four.js
+++ src/components/Graph_four.js
@@ -5,48 +5,9 @@ import './Graph.css';
 
 
 class Graph_four extends React.Component {
-    // options = {
-    //     chart: {
-    //       type: "bar"
-    //     },
-    //     yAxis : {
-    //       title: {
-    //          text: 'Demand'
-    //       }
-    //     },
-    //     xAxis : {
-    //       categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-    //          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    //    },
-      
-    //     title: {
-    //       text: "Car Color Vs Demand"
-    //     },
-    //     series :  [{
-    //       name: 'Red',
-    //       color : 'Red',
-    //       data: [{y:7, color: 'red'},{y: 3, color: 'red'},{y: 84, color: 'red'}]
-    //    }, 
-    //    {
-    //       name: 'lightgray',
-    //       color: 'lightgray',
-    //       data: [{y: 3, color: 'lightgray'},{y: 2, color: 'lightgray'},{y: 8, color: 'lightgray'}]
-    //    }, 
-    //    {
-    //       name: 'black',
-    //     color: 'black',
-    //       data: [{y: 7, color: 'black'},{y: 63, color: 'black'},{y: 94, color: 'black'}]
-    //    }, 
-    //    {
-    //       name: 'blue',
-    //       color: 'blue',
-    //       data: [{y: 24, color: 'blue'},{y: 34, color: 'blue'},{y: 8, color: 'blue'}]
-    //    }
-    // ]
-    //   };
-    
+        
    options = {
-     colors: ['#000', 'blue', 'Red', 'Silver','Yellow'],
+     colors: ['#000', 'DarkBlue', 'tomato', 'Silver','Yellow'],
 
     chart: {
         type: 'pie'
@@ -64,7 +25,7 @@ class Graph_four extends React.Component {
     series: [{
         data: [
             ['black', 44.2],
-            ['blue',     26.6],
+            ['Blue',     26.6],
             ['Red',     20],
             ['Silver',  3.1],
             ['Other',   5.4]
