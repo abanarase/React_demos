460277a580d6310325a10bf43b72faef611103f3
unknown <amruta.banarase@saama.com>
2020-04-15 14:28:46 +0530

    Latest axios update

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,40 +1,56 @@
 
 import React, { useState, useEffect, memo } from 'react';
 import Table from "./Table";
+import axios from 'axios';
+
 const headers = ["Name","Car model","Total Amount","Days"];
 
 const UserList = () => {
-  const [usered, setUserList] = useState([]);
+  const [users, setUserList] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-  // const [temp, setTemp] = useState(`${data}, Yagnesh`);
 
   
-  const apiCall = (url, reqInit = {}) => {
-    setLoading(true);
-    return new Promise((resolve, reject) => {
-      fetch(url, reqInit)
-        .then((res) => res.json())
-        .then((data) => {
-          resolve(data);
-        })
-        .catch((err) => {
-          setError(err);
-          reject(err);
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-    });
-  };
+  // const apiCall = (url, reqInit = {}) => {
+  //   setLoading(true);
+  //   return new Promise((resolve, reject) => {
+  //     fetch(url, reqInit)
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         resolve(data);
+  //       })
+  //       .catch((err) => {
+  //         setError(err);
+  //         reject(err);
+  //       })
+  //       .finally(() => {
+  //         setLoading(false);
+  //       });
+  //   });
+  // };
 
-  const loadData = async () => {
-    const data = await apiCall('http://localhost:3001/todos');
-    console.log("updated data",JSON.stringify(data));
-    setUserList(data);
+  // const loadData = async () => {
+  //   const data = await apiCall('http://localhost:3001/todos');
+  //   console.log("updated data",JSON.stringify(data));
+  //   setUserList(data);
     
-  };
-
+  // };
+  const loadData=()=>{
+    setLoading(true);
+  axios.get('http://localhost:3001/todos')
+  .then(res => {
+    if(res!=error){
+      setLoading(false);
+      const data = res.data;
+      setUserList(data);
+    }
+    else{
+      setLoading(false);
+      setError(res);
+    }
+    
+  })
+}
   // componentDidMount
   useEffect(() => {
     loadData();
@@ -50,7 +66,7 @@ const UserList = () => {
   }
 
   return(
-    <Table thead={headers} tbody={usered}/>
+    <Table thead={headers} tbody={users}/>
    );
 };
 
