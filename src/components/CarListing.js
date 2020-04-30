60019bb0be7aa12e22438544bfdb8509bd917552
unknown <amruta.banarase@saama.com>
2020-04-30 10:32:05 +0530

    Search Filter applied on car list

--- src/components/CarListing.js
+++ src/components/CarListing.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect, memo } from 'react';
 import Table from "./Table";
 import data from  '../Data/Carlist.json';
 const theader =[ "ID","Car model","Registration no","Transmission","Fuel","Displacement_CC","Mileage in km",
@@ -6,8 +6,9 @@ const theader =[ "ID","Car model","Registration no","Transmission","Fuel","Displ
 
 
   const CarListing = (props) =>{
-
+   const [carList, setCarList] = useState([]);
    const generateHeader= ()=>{
+      console.log("in re");
       let columnHeader = theader;  
        let res=[];
        for(var i =0; i < columnHeader.length; i++){
@@ -15,33 +16,53 @@ const theader =[ "ID","Car model","Registration no","Transmission","Fuel","Displ
        }
        return res;
      }
-     const generatebody= ()=> {  	
-      let columnbody = data ;   	
-        const  newdata= columnbody.map((data) =>{
+     const generatebody= (carobj)=> {  	
+      let columnbody = carobj ;   	
+        const  newdata= columnbody.map((carobj) =>{
          return (
-             <tr key={data.id}>
-                <td>{data.id}</td>
-                <td>{data.car_model}</td>
-                <td>{data.registration_no}</td>
-                <td>{data.transmission}</td>
-                <td>{data.Fuel}</td>
-                <td>{data.Displacement_CC}</td>
-                <td>{data.Mileage_in_km}</td>
-                <td>{data.Color}</td>
-                <td>{data.Rent_per_km}</td>              
-                <td>{data.Booking_status}</td>          
-               <td><input className="Erra" type="button" value="Book" id={data.id}  onClick={e =>props.action.childHandler(e, "id")}/></td>
+             <tr key={carobj.id}>
+                <td>{carobj.id}</td>
+                <td>{carobj.car_model}</td>
+                <td>{carobj.registration_no}</td>
+                <td>{carobj.transmission}</td>
+                <td>{carobj.Fuel}</td>
+                <td>{carobj.Displacement_CC}</td>
+                <td>{carobj.Mileage_in_km}</td>
+                <td>{carobj.Color}</td>
+                <td>{carobj.Rent_per_km}</td>              
+                <td>{carobj.Booking_status}</td>          
+               <td><input className="Erra" type="button" value="Book" id={carobj.id}  onClick={e =>props.action.childHandler(e, "id")}/></td>
  
              </tr>
           )
        });
        return newdata;
       }
+      const loadData = async () => {
+         const updata = generatebody(data);
+         setCarList(updata);
+       };
+       const filterList=(e)=>{
+         let alpha=e.target.value;
+         let mapItem =  data.filter(function(item) {
+            let car= item.car_model
+            return car.toLowerCase().indexOf(alpha.toLowerCase()) !== -1
+         });
+         const updatedList=generatebody(mapItem);
+         setCarList(updatedList);         
+      }
+
+      useEffect(() => {
+         loadData();
+       }, []);
       
-        return (        
+        return ( 
            <div>    
               <h1 id='title'>Car List</h1>
-              <Table thead={generateHeader()} tbody={generatebody()}/>              
+              <fieldset className="form-group col-3">
+        <input type="text" className="form-control form-control-lg" placeholder="Search car" onChange={filterList}/>
+        </fieldset>
+              <Table thead={generateHeader()} tbody={carList}/>              
            </div>
         )
      
